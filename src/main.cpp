8ebfab51f5515005a336c1b72daa48fb3f1aaa8d
Sedfer <balovilya@mail.ru>
2017-06-16 18:39:29 +0300

    Main() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <signal.h>
 using namespace std;
 
-int main()
+bool cca(ifstream &file);
+
+void sigintHandler(int param)
 {
-  cout << "This is CCA Sat-Solver" << endl;
+  cout << "UNKNOWN" << endl;
+  exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+  signal(SIGINT, sigintHandler);
+
+  if(argc < 2){
+    cout << "File is not specified!" << endl;
+    return 0;
+  }
+  
+  ifstream file(argv[1]);
+  if(!file.is_open()){
+    cout << "Can not open file!" << endl;
+    return 0;
+  }
+  
+  bool res = cca(file);
+  if(res){
+    cout << "SATISFIABLE" << endl;
+  }
+  else{
+    cout << "UNSATISFIABLE" << endl;
+  }
+}
+
+bool cca(ifstream &file)
+{
+  file.close();
+  
+  return false;
 }
